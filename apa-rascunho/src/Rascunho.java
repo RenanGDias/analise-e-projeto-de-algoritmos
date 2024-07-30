7e19f9637ae125a48872aa9a74ad16c4bba1ae09
Renan Dias <129848338+RenanGDias@users.noreply.github.com>
2024-07-30 17:07:46 -0300

    Pequena alteração
    Nome do arquivo e da classe alterados

---
+++ apa-rascunho/src/Rascunho.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Rascunho {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Qual posição da sequência de fibonnaci você quer?");
+        int output = scanner.nextInt();
+        int resposta = 0;
+
+        try {
+            resposta = fibonnaci(output - 1);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erro: " + e.getMessage());
+            scanner.close();
+            return;
+        }
+
+        System.out.println("A resposta é: " + resposta);
+        scanner.close();
+    }
+
+    public static int fibonnaci(int n) {
+
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else if (n < 0) {
+            throw new IllegalArgumentException("Índice nulo ou negativo não permitido!");
+        }
+
+        return fibonnaci(n - 1) + fibonnaci(n - 2);
+    }
+}
